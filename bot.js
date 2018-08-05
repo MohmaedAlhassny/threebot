8380b0c5db272cbb9485b73395c6e66e81a09bd8
Mohmaed Ayad <40663543+MohmaedAlhassny@users.noreply.github.com>
2018-08-05 17:02:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -259,4 +259,15 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
 
 
    
+const Myacc = new Discord.Client();
+
+Myacc.on('ready', () => {
+	Myacc.user.setGame('أخسر العالم كلها، لاتخسر الي يحبك.');
+	console.log(`${Myacc.guilds.size}server, ${Myacc.users.size}user.`);
+});
+
+Myacc.login(process.env.Mohmaed);
+
+
+
 client.login(process.env.TOKEN);
